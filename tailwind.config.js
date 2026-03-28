eff38b28ed539a6994e825b794dbff446effdd44
vtion001 <generalmanager.tiongsons@gmail.com>
2026-03-29 03:24:54 +0800

    fix: restore Inertia/React setup after Breeze installation

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+export default {
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/js/**/*.{js,jsx,ts,tsx}",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
