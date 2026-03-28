cfeeb5365a673590e0f788d0474f869021a8ef13
vtion001 <generalmanager.tiongsons@gmail.com>
2026-03-29 03:20:36 +0800

    feat: configure Inertia root template and middleware

---
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Illuminate\Http\Request;
+use Inertia\Middleware;
+
+class HandleInertiaRequests extends Middleware
+{
+    public function share(Request $request): array
+    {
+        return array_merge(parent::share($request), [
+            'auth' => $request->user() ? [
+                'id' => $request->user()->id,
+                'name' => $request->user()->name,
+                'email' => $request->user()->email,
+            ] : null,
+        ]);
+    }
+}
